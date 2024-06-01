418f06c5a57239e6f63aef698de1a297a0a7cfc1
John Hileman <106003582+jhileman07@users.noreply.github.com>
2024-06-01 17:36:59 -0500

    implemented RDS and previous messages are stored

--- server/go.mod
+++ server/go.mod
@@ -4,10 +4,13 @@ go 1.21.0
 
 require (
 	github.com/aws/aws-sdk-go v1.53.10
+	github.com/go-sql-driver/mysql v1.8.1
 	github.com/gorilla/websocket v1.5.1
+	github.com/joho/godotenv v1.5.1
 )
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
 )
--- server/room.go
+++ server/room.go
@@ -3,9 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/translate"
 )
 
@@ -14,22 +14,23 @@ type Room struct {
 	broadcast chan []byte
 	register  chan *Client
 	remove    chan *Client
+	id        int
+	messages  []byte
 }
 
-func newRoom() *Room {
+func newRoom(id int) *Room {
 	return &Room{
 		clients:   make(map[*Client]bool),
 		broadcast: make(chan []byte),
 		register:  make(chan *Client),
 		remove:    make(chan *Client),
+		id:        id,
+		messages:  []byte{},
 	}
 }
 
 func (room *Room) translateMessage(message []byte) {
 	// Initialize translation session once
-	translateSession := translate.New(session.Must(session.NewSession(&aws.Config{
-		Region: aws.String("us-east-2"),
-	})))
 
 	var msg map[string]string
 
@@ -42,6 +43,13 @@ func (room *Room) translateMessage(message []byte) {
 	originalLanguage := msg["language"]
 	originalMessage := msg["message"]
 	senderUsername := msg["username"]
+
+	// store message to database
+	_, err = db.Exec("INSERT INTO messages (room_id, text, original_language, creator_name, created_at) VALUES (?, ?, ?, ?, ?);", room.id, originalMessage, originalLanguage, senderUsername, time.Now())
+
+	if err != nil {
+		fmt.Println("Error storing into DB")
+	}
 
 	for client := range room.clients {
 		receiverLanguage := client.language
@@ -93,3 +101,49 @@ func (room *Room) runRoom() {
 		}
 	}
 }
+
+func (room *Room) getMessages() {
+	roomid := room.id
+
+	// query for messages
+	rows, err := db.Query("SELECT * FROM messages WHERE room_id = ? ORDER BY created_at ASC;", roomid)
+	if err != nil {
+		fmt.Println("Error querying messages: ", err)
+		return
+	}
+	defer rows.Close()
+
+	var messages []map[string]string
+
+	for rows.Next() {
+		var id int
+		var room_id int
+		var text string
+		var original_language string
+		var creator_name string
+		var created_at []uint8
+
+		err = rows.Scan(&id, &room_id, &text, &original_language, &creator_name, &created_at)
+		if err != nil {
+			fmt.Println("Error scanning messages: ", err)
+			return
+		}
+
+		msg := map[string]string{
+			"username": creator_name,
+			"language": original_language,
+			"message":  text,
+		}
+
+		messages = append(messages, msg)
+	}
+
+	data, err := json.Marshal(messages)
+	if err != nil {
+		fmt.Println("Error marshalling messages: ", err)
+		return
+	}
+
+	room.messages = data
+}
+
